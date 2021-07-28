07d7eed5b0b7f4dc74f2d1345c9f65cbeec8d031
Vlad <44240482+caraseli02@users.noreply.github.com>
2021-07-28 16:40:37 +0200

    PR Final
    Gracias por readme tips

--- test/test01.js
+++ test/test01.js
@@ -1,4 +1,16 @@
 function geekshubs(index) {
-    return "";
+  let output = "";
+  for (var i = 1; i <= index; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      output += "GeeksHubs\n";
+    } else if (i % 3 === 0) {
+      output += "Geeks\n";
+    } else if (i % 5 === 0) {
+      output += "Hubs\n";
+    } else {
+      output += `${i}\n`;
+    }
+  }
+  return output;
 }
 module.exports = geekshubs;
